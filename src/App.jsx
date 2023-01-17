77d73423efdda9caf206379be7d7e17dc95c38d7
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-17 05:11:53 -0300

    pokemon Id

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./components/Button";
+import { useState } from "react";
 
 const App = () => {
+  const [pokemonId, setPokemonId] = useState(1);
+
   return (
     <div>
       <Button
         text="Anterior"
         clicked={() => {
-          console.log("clicked Anterior");
+          setPokemonId(pokemonId - 1);
         }}
       />
       <Button
         text="Siguiente"
         clicked={() => {
-          console.log("clicked Siguiente");
+          setPokemonId(pokemonId + 1);
         }}
       />
     </div>
