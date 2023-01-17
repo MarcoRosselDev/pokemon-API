0d508100f925bde687655c947f7953b3a2c95e30
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-17 04:30:59 -0300

    render metod  2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 // nesecitamos crear un elemento raiz
 
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 console.log(root);
 
+root.render(<App />);
 // de este root nos interesa el metodo render que es para dibujar
