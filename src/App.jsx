bca47413362a6b50d9e17bb4b53fb67973acfd76
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-17 22:14:56 -0300

    crean function

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,19 @@ import { Card } from "./components/Card";
 const App = () => {
   const [pokemonId, setPokemonId] = useState(1);
 
+  function prevClick() {
+    pokemonId === 1 ? setPokemonId(1) : setPokemonId(pokemonId - 1);
+  }
+  function nextClick() {
+    setPokemonId(pokemonId + 1);
+  }
+
   return (
     <div>
       <Card />
-      <Button
-        text="Anterior"
-        clicked={() => {
-          // if (pokemonId === 1) {
-          //   setPokemonId(1);
-          // } else {
-          //   setPokemonId(pokemonId - 1);
-          // }
-
-          // es mejor con ternarios:
-
-          pokemonId === 1 ? setPokemonId(1) : setPokemonId(pokemonId - 1);
-        }}
-      />
+      <Button text="Anterior" clicked={() => prevClick()} />
       {pokemonId}
-      <Button
-        text="Siguiente"
-        clicked={() => {
-          setPokemonId(pokemonId + 1);
-        }}
-      />
+      <Button text="Siguiente" clicked={() => nextClick()} />
     </div>
   );
 };
